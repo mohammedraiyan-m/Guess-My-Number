7a50cb5bcec443702665f61cf98e843f2e425512
mohammedraiyan-m <mohammedraiyan456@gmail.com>
2024-09-20 19:15:38 +0530

    new line of command added

--- script.js
+++ script.js
@@ -14,12 +14,12 @@ document.querySelector('.check').addEventListener('click', function () {
 
   // When there is no input
   if (!guess) {
-    // document.querySelector('.message').textContent = '⛔️ No number!';
+    
     displayMessage('⛔️ No number!');
 
     // When player wins
   } else if (guess === secretNumber) {
-    // document.querySelector('.message').textContent = '🎉 Correct Number!';
+    
     displayMessage('🎉 Correct Number!');
     document.querySelector('.number').textContent = secretNumber;
 
@@ -30,16 +30,15 @@ document.querySelector('.check').addEventListener('click', function () {
       highscore = score;
       document.querySelector('.highscore').textContent = highscore;
     }
+     // When guess is wrong
   } 
   else if (guess !== secretNumber) {
     if (score > 1) {
-      // document.querySelector('.message').textContent =
-      // guess > secretNumber ? '📈 Too high!' : '📉 Too low!';
       displayMessage(guess > secretNumber ? '📈 Too high!' : '📉 Too low!');
       score--;
       document.querySelector('.score').textContent = score;
     } else {
-      // document.querySelector('.message').textContent = '💥 You lost the game!';
+      
       displayMessage('💥 You lost the game!');
       document.querySelector('.score').textContent = 0;
     }
